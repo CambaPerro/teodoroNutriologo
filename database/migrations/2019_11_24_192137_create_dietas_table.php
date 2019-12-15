b9dc81cac24c03897761313244f35691127d9863
johnalba♠ <brajhan3@hotmail.com>
2019-12-15 18:19:42 -0400

    modificado actividad, tipoactividad y nivel de actividad2

--- database/migrations/2019_11_24_192137_create_dietas_table.php
+++ database/migrations/2019_11_24_192137_create_dietas_table.php
@@ -16,6 +16,7 @@ class CreateDietasTable extends Migration
         Schema::create('dietas', function (Blueprint $table) {
             $table->bigIncrements('id');
             $table->unsignedBigInteger('id_usuario');
+            $table->unsignedBigInteger('id_nivel');
             $table->unsignedBigInteger('id_actividad')->nullable();
             $table->date('fecha_inicio');
             $table->date('fecha_fin');
@@ -25,7 +26,7 @@ class CreateDietasTable extends Migration
             $table->string('tipo',20);
             $table->foreign('id_usuario')->references('id')->on('usuarios');
             $table->foreign('id_actividad')->references('id')->on('actividad_fisicas');
-            
+            $table->foreign('id_nivel')->references('id')->on('nivel_actividads');
             // $table->timestamps();
         });
     }
