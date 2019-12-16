dc2f2c1c0bd488f9a84c35cec77d50b78c78c181
Luis Rodrigo Abasto <vida12345abasto@gmail.com>
2019-12-16 01:44:42 -0400

    abasto

--- resources/views/sidebar/sidebar.blade.php
+++ resources/views/sidebar/sidebar.blade.php
@@ -8,6 +8,37 @@
                     Gestion de Dieta
                 </li>
                 <li class="nav-item nav-dropdown">
+                    <a class="nav-link nav-dropdown-toggle" href="#"><i class="nav-icon icon-people"></i>Usuarios</a>
+                    <ul class="nav-dropdown-items">
+                        <li class="nav-item">
+                            <a class="nav-link" href="dietas"><i class="nav-icon icon-user"></i> Dieta</a>
+                        </li>
+                    </ul>
+                    <ul class="nav-dropdown-items">
+                        <li class="nav-item">
+                            <a class="nav-link" href="alimento_dietas"><i class="nav-icon icon-user"></i> Alimento Dieta</a>
+                        </li>
+                    </ul>
+                    <ul class="nav-dropdown-items">
+                        <li class="nav-item">
+                            <a class="nav-link" href="menus"><i class="nav-icon icon-grid"></i> Menu</a>
+                        </li>
+                    </ul>
+                    <ul class="nav-dropdown-items">
+                        <li class="nav-item">
+                            <a class="nav-link" href="register"><i class="nav-icon icon-user"></i> Registro de Usuario</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="login"><i class="nav-icon icon-user"></i> Login</a>
+                        </li>
+                    </ul>
+                </li>
+                @guest
+                
+                @else
+                @if (Auth::user()->tipo=='A')
+
+                <li class="nav-item nav-dropdown">
                     <a class="nav-link nav-dropdown-toggle" href="#"><i class="nav-icon icon-bag"></i> Administracion</a>
                     <ul class="nav-dropdown-items">
                         <li class="nav-item">
@@ -39,37 +70,9 @@
                             <a class="nav-link" href="menus"><i class="nav-icon icon-grid"></i> Menu</a>
                         </li>
                     </ul>
-                  
-                    
-                </li>    
-                <li class="nav-item nav-dropdown">
-                        <a class="nav-link nav-dropdown-toggle" href="#"><i class="nav-icon icon-people"></i>Usuarios</a>
-                        <ul class="nav-dropdown-items">
-                            <li class="nav-item">
-                                <a class="nav-link" href="dietas"><i class="nav-icon icon-user"></i> Dieta</a>
-                            </li>
-                        </ul>
-                        <ul class="nav-dropdown-items">
-                            <li class="nav-item">
-                                <a class="nav-link" href="alimento_dietas"><i class="nav-icon icon-user"></i> Alimento Dieta</a>
-                            </li>
-                        </ul>
-                        <ul class="nav-dropdown-items">
-                            <li class="nav-item">
-                                <a class="nav-link" href="menus"><i class="nav-icon icon-grid"></i> Menu</a>
-                            </li>
-                        </ul>
-                        <ul class="nav-dropdown-items">
-                            <li class="nav-item">
-                                <a class="nav-link" href="register"><i class="nav-icon icon-user"></i> Registro de Usuario</a>
-                            </li>
-                            <li class="nav-item">
-                                <a class="nav-link" href="login"><i class="nav-icon icon-user"></i> Login</a>
-                            </li>
-                        </ul>
-                    </li>
-               
-                
+                </li>  
+                @endif
+                @endguest                
             </ul>
         </nav>
         <button class="sidebar-minimizer brand-minimizer" type="button"></button>
